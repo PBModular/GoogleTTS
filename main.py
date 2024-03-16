541490bdb7b92e1d38c02bbed2ab3987c2d0a8cc
Ultra119 <64977575+Ultra119@users.noreply.github.com>
2024-03-16 14:18:35 +0200

    Initialize basic gtts module

---
+++ main.py
@@ -0,0 +1,44 @@
+from base.mod_ext import BaseModule
+from base.module import command
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
+from gtts import gTTS
+from io import BytesIO
+import tempfile
+
+class GoogleTTSModule(BaseModule):
+    @command("tts")
+    async def tts_cmd(self, bot: Client, message: Message):
+        """Convert text to speech with Google APIs"""
+
+        text = await self.get_text_from_message(message)
+        
+        if not text:
+            await message.reply(self.S["no_text"])
+            return
+
+        print(f"Input text: {text}")
+        tts = gTTS(text=text, lang='en')
+        speech_bytes = BytesIO()
+        tts.write_to_fp(speech_bytes)
+        speech_bytes.seek(0)
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp:
+            temp_filename = temp.name
+            temp.write(speech_bytes.read())
+
+        await message.reply_voice(voice=temp_filename)
+
+    async def get_text_from_message(self, message):
+        """Extract text from the message or the replied message"""
+        text = message.text.split(" ", 1)
+        
+        if message.reply_to_message:
+            text = message.reply_to_message.text or ""
+        elif len(text) > 1:
+            text = text[1]
+        else:
+            text = ""
+            
+        return text.strip()
